fad698b40babd62bb1e7fe3bbf404beb2c5aff9c
arkhipushka <104166693+arkhipushka@users.noreply.github.com>
2022-06-29 01:10:23 +0300

    Add files via upload

---
+++ js/task1/string-filter.js
@@ -0,0 +1,26 @@
+const string = "Привет! Как дела?";
+
+const vowels = ["а", "е","ё", "и","о", "у","я", "ы"]
+
+const getVowels = stringToFilter =>{
+    let extractedVowels = "";
+
+
+    for(let i=0; i<stringToFilter.length; i++){
+        const currentLetter = stringToFilter[i].toLowerCase();
+
+    if (vowels.includes(currentLetter)){
+        extractedVowels += currentLetter;
+    }
+
+        
+
+        
+    }
+    return extractedVowels;
+}
+
+
+console.log(getVowels(string));
+
+     
